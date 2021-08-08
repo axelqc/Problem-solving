16f9666636c04a6232935f4d32dd1c7974e48736
Axel Quiroga Caldera <87676595+axelqc@users.noreply.github.com>
2021-08-08 11:59:51 -0500

    Add files via upload

---
+++ Ex 3.py
@@ -0,0 +1,14 @@
+print('''
+Take a list, say for example this one:
+    a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+and write a program that prints out all the elements of the list that are less than 5.
+    ''')
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+for b in a:
+    if b < 5:
+        print(b)
+
+print('''
+    source: https://www.practicepython.org/exercise/2014/02/15/03-list-less-than-ten.html
+    ''')
